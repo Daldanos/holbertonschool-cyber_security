547a45021ace0f86c06aef5572a7df3e9a1c64c6
German Silveira <adolfgsilveira@gmail.com>
2024-07-31 12:45:05 -0400

    Making Changes

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -u "$1" -o user,pid,pcpu,pmem,vsz,rss,tty,stat,start,time,cmd | awk 'NR>1 && $5>0 && $6>0'
+ps -u "$1" -o user,pid,pcpu,pmem,vsz,rss,tty,stat,start,time,cmd --no-headers | awk '$5>0 && $6>0'
