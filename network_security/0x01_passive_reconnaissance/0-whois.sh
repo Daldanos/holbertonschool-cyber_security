a376d97dbd5bb3c956dc21026b42bb30320dbc22
German Silveira <adolfogsilveira@gmail.com>
2024-08-17 17:50:39 -0400

    Making Changes

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^Registrant|^Admin|^Tech/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); printf "%s,%s%s\n", $1, $2, (length($2) % 2 ? " " : "")}' OFS=',' > "$1.csv"
+whois $1 | awk -F': ' '/^Registrant|^Admin|^Tech/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); printf "%s,%s%s\n", $1, $2, (length($2) % 2 ? "" : "")}' OFS=',' > "$1.csv"
