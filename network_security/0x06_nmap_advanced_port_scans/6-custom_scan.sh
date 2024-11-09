a810d6264b74c3fecfe6d3c99bcce9888292fd1b
German Silveira <adolfogsilveira@gmail.com>
2024-11-09 16:03:04 -0500

    Making Changes

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -p $2 --scanflags SYN,ACK,FIN,RST,URG,PSH -oN custom_scan.txt $1 &> /dev/null
+sudo nmap -p $2 --scanflags SYN,ACK,FIN,RST,URG,PSH -oN custom_scan.txt $1 &>/dev/null
