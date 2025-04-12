ea52defdce2e18e826570b33024b651e03727601
German Silveira <adolfogsilveira@gmail.com>
2025-04-12 15:28:41 -0400

    Making Changes

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "sshd" auth.log | grep "Accepted" | awk '{for (i=1; i<=NF; i++) if ($i=="from") print $(i+1)}' | sort -u | wc -l
+awk '/Failed password|Accepted password/ {print $11}' auth.log | sort -u | wc -l
