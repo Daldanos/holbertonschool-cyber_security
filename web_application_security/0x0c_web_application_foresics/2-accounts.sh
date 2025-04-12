ead614c519e9ad10004dc13572bece9555b3e8a9
German Silveira <adolfogsilveira@gmail.com>
2025-04-12 15:02:28 -0400

    Making Changes

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-tail -n 1000 auth.log | awk '/Failed password/ {fail[$NF]++} /Accepted password/ {success[$NF]++} END {for (u in success) if (fail[u]) print u}'
+tail -n 1000 auth.log | awk '
+/Failed password/ {
+    for (i=1; i<=NF; i++) if ($i == "for") fail[$(i+1)]++
+}
+/Accepted password/ {
+    for (i=1; i<=NF; i++) if ($i == "for") success[$(i+1)]++
+}
+END {
+    for (user in success)
+        if (fail[user]) print user
+}'
+
