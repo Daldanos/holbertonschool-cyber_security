45536cc681f636b3f63c6d12834c3f9ceb2ac1b3
German Silveira <adolfogsilveira@gmail.com>
2025-04-12 15:22:07 -0400

    Making Changes

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-num_atacantes=$(awk '/Failed password/ {print $11}' auth.log | sort -u | wc -l)
-echo $num_atacantes
+ips=$(awk '/Accepted password/ || /Failed password/ {print $11}' auth.log)
+unique_ips=$(echo "$ips" | sort -u)
+num_atacantes=$(echo "$unique_ips" | wc -l)
+echo "$num_atacantes"
