3e9272bab5725e778f31a4328cf2d71f24fdc3a0
German Silveira <adolfogsilveira@gmail.com>
2025-04-12 16:17:12 -0400

    Making Changes

--- network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-nmap -p 21,22,80,443,9929,31337 \
---script http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon \
--oN comprehensive_scan_results.txt "$1"
+nmap -p 21,22,80,443,9929,31337 --script http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon -oN comprehensive_scan_results.txt "$1"
