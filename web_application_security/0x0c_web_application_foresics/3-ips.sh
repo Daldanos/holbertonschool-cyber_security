ce8d9a8654943cc85619211472906699809b5820
German Silveira <adolfogsilveira@gmail.com>
2025-04-12 15:40:49 -0400

    Making Changes

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-awk '/Failed password|Accepted password/ {print $11}' auth.log | sort -u | wc -l
+if [ $# -ne 1 ]; then
+    echo "Uso: $0 <auth.log>"
+    exit 1
+fi
+
+grep "Accepted password" "$1" | awk '{print $11}' | sort -u | wc -l
