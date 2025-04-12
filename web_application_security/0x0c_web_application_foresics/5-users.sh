c52aead3e6d0c5529bc7213a24c38e5ed80d7c72
German Silveira <adolfogsilveira@gmail.com>
2025-04-12 15:59:40 -0400

    Making Changes

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E "new user|useradd" auth.log | awk -F'=' '{print $2}' | awk -F',' '{print $1}' | sort -u | tr '\n' ',' | sed 's/,$//'
+grep -E "new user|useradd" auth.log | awk -F'=' '{print $2}' | awk -F',' '{print $1}' | sort -u | tr '\n' ',' | sed 's/,$//' && echo
