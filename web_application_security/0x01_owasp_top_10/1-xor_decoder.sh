afeaa6347818a30028aea7be3a447b521403ac6e
German Silveira <adolfogsilveira@gmail.com>
2025-07-13 17:43:30 -0400

    Making Changes

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -n "${1#*xor}" | base64 -d | while IFS= read -n1 -r c; do printf "\x$(printf '%x' $(( $(printf '%d' "'$c") ^ 53 ))"; done
+echo -n "${1#*xor}" | base64 -d | while IFS= read -n1 c; do printf "\\$(printf '%03o' "$(( $(printf '%d' "'$c") ^ 53 ))")"; done
 echo
