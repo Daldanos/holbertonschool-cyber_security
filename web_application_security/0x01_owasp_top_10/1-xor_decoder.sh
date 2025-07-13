c350cd316b5ca1a562aaf64184efab5b51dd0851
German Silveira <adolfogsilveira@gmail.com>
2025-07-13 17:54:29 -0400

    Making Changes

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-b64=${1#*\}} # elimina todo hasta la primera }
-echo "$b64" | base64 -d 2>/dev/null | od -An -t u1 | tr -s ' ' | \
-while read -a bytes; do
-  for key in {1..255}; do
-    out=""
-    for b in "${bytes[@]}"; do
-      out+=$(printf "\\x%02x" $((b ^ key)))
-    done
-    [[ $out == "yosri_check_value" ]] && echo "ok" && exit 0
-  done
-done
-echo "invalid"
+echo ${1#*\}} | base64 -d 2>/dev/null | od -An -t u1 | awk '{for(i=1;i<=NF;i++)printf "%c", $i^53}'
