59c27c54dda2076d188ad7cd524ad942d753538e
German Silveira <adolfogsilveira@gmail.com>
2025-08-31 19:21:21 -0400

    Making Changes

---
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# Command-line arguments printer for Holberton School
+
+def print_arguments
+  if ARGV.empty?
+    puts "No arguments provided."
+  else
+    ARGV.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
+    end
+  end
+end
