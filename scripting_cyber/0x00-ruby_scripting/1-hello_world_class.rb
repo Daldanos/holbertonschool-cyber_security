accf3889aa5a53c8b0048d2977fd7e483a2e34f7
German Silveira <adolfogsilveira@gmail.com>
2025-08-31 18:51:35 -0400

    Making Changes

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# Hello World class in Ruby for Holberton School
+
+class HelloWorld
+  def initialize
+    @message = "Hello, World!"
+  end
+
+  def print_hello
+    puts @message
+  end
+end
